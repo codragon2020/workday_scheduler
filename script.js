9225b428d9ada312a895cf3d2a34bb72cc24a1ec
Code JSON <73917308+codragon2020@users.noreply.github.com>
2020-12-11 07:00:18 -0500

    Removed the moment(Date) causing a deprecation error. Added the 9am block code with save button. Button does not save yet.

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 // Moment declaration for day and time is positioned in header
-moment(Date);
-$("#currentDay").text(moment().format('dddd MMMM Do YYYY, h:mm a'));
+// moment(Date);
+$("#currentDay").text(moment().format('DD MM YYYY hh:mm:ss'));
+
+// Calculates the start of day + 9 to return 9 am
+var beforeTime = moment().startOf('day').add(9, "hours");
+
+
+// Time blocks
+// 9 AM 
+var time1 = beforeTime.add(0, "h");
+// var time1 = beforeTime;
+time1 = time1.format('hh:mm a');
+// Populates time formula into html
+$(".block1").text(time1);
